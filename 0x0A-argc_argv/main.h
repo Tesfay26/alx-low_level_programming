9a84b4688690043137bd2848ef29341e25031e6c
Tesfay26 <tesfaybsrat26@gmail.com>
2023-01-11 11:52:10 -0800

    argc and argv

---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * This header file contains all the prototypes required for the
+ * 0x0A-argc_argv project.
+ *
+ * @author: Gizachew Bayness
+ *
+ * Date crated: April 29, 2022
+ */
+
+int _putchar(char c);
+
+#endif
