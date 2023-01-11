c3052442b5da673ebf247963308a5d969c18ca2d
Tesfay26 <tesfaybsrat26@gmail.com>
2023-01-11 11:25:25 -0800

    Static_Libraries

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,3 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * This header file contains all the prototypes required for the
+ * 0x09-static_libraries project.
+ *
+ * @author: Gizachew Bayness
+ *
+ * Date crated: April 29, 2022
+ */
+
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -18,3 +30,5 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
+
+#endif
